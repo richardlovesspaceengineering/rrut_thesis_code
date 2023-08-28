110db71dca2fd7147ccc64b5e3b83c830bc7ccd8
Richard Rutherford <rrut5116@uni.sydney.edu.au>
2023-08-28 22:02:48 +1000

    Finished cv_mdl.

--- ISA-CMOP_Python/features/cv_mdl.py
+++ ISA-CMOP_Python/features/cv_mdl.py
@@ -1,12 +1,25 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
+
 def cv_mdl(objvar, decvar):
     """
     Fit a linear model to decision variables-constraint violation, then take the R2 and difference between the max and min of the absolute values of the linear model coefficients.
     """
-    
+
     # Fit linear model to decision variable, CV data.
-    mdl = 
-    
+    mdl = LinearRegression().fit(decvar, objvar)
+
+    # R2 (adjusted).
+    num_obs = objvar.shape[0]
+    num_coef = objvar.shape[1]
+    r2_unadj = mdl.score(decvar, objvar)
+    mdl_r2 = 1 - (1 - r2_unadj) * (num_obs - 1) / (num_obs - num_coef - 1)
+
+    # Range. Ignore the intercepts.
+
+    # Why isn't this taking individual absolute values as the paper suggests? Maybe because all coefficients are positive?
+
+    range_coeff = max(mdl.coef[1:]) - min(mdl.coef[1:])
+
     return [mdl_r2, range_coeff]
--- ISA-CMOP_Python/scratch.py
+++ ISA-CMOP_Python/scratch.py
@@ -2,7 +2,9 @@ import numpy as np
 
 # cv_distr scratch working.
 A = np.array([[2 + 2j, -2j], [3, 6]])
+print(A.shape)
 rmag = np.nonzero(np.sum(np.imag(A) != 0, axis=1))
 print(rmag)
 A = np.delete(A, rmag, axis=0)
 print(A)
+print(A.shape)
