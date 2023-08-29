6ec779d433ed6b79da16820632b8ddaab64a2c3f
Richard Rutherford <rrut5116@uni.sydney.edu.au>
2023-08-29 10:04:20 +1000

    f_corr working.

--- ISA-CMOP_Python/features/f_corr.py
+++ ISA-CMOP_Python/features/f_corr.py
@@ -1,3 +1,31 @@
-def f_corr(objvar):
-    """ """
+import scipy
+import numpy as np
+import warnings
+
+
+def f_corr(objvar, significance_level=0.05):
+    """
+    Significant correlation between objective values.
+
+    Verel2013 provides a detailed derivation, but it seems as if we assume the objective correlation is the same metween all objectives. That is, for a correlation matrix C, C_np = rho for n != p, C_np = 1 for n = p. We want the value of rho.
+
+    Since correlations are assumed to be equal across all objectives, we can just compute one pairwise correlation coefficient. Alsouly finds the same but computes the symmetric 2x2 correlation coefficient and pvalue matrices before extracting the upper-right value.
+    """
+
+    with warnings.catch_warnings():
+        # Suppress warnings where corr is NaN - will just set to 0 in this case.
+        warnings.simplefilter("ignore", scipy.stats.ConstantInputWarning)
+        result = scipy.stats.pearsonr(objvar[:, 0], objvar[:, 1])
+
+    corr_obj = result.statistic
+    pvalue = result.pvalue
+
+    # Signficance test. Alsouly does p > alpha for some reason.
+    if pvalue < significance_level:
+        corr_obj = 0
+    elif np.isnan(corr_obj):
+        corr_obj = 0
+
+    # Make correlation 0 if there is no change in one vector.
+
     return corr_obj
--- ISA-CMOP_Python/scratch.py
+++ ISA-CMOP_Python/scratch.py
@@ -1,4 +1,5 @@
 import numpy as np
+from features.f_corr import f_corr
 
 # cv_distr scratch working.
 A = np.array([[2 + 2j, -2j], [3, 6]])
@@ -8,3 +9,10 @@ print(rmag)
 A = np.delete(A, rmag, axis=0)
 print(A)
 print(A.shape)
+
+from scipy.stats import pearsonr
+
+matrix = np.array([[6, -3, -2, 4], [6, 6, 6, 6], [6, 5, 6, 9]])
+# result = np.corrcoef(matrix[0, :], matrix[1, :])
+print(matrix)
+print(f_corr(matrix))
