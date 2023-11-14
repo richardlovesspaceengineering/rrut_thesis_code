43c5319205a4d01c0bab85ef4238cc133e239db5
personal <richardrutherford@outlook.com>
2023-11-14 11:44:01 +1100

    Set number of samples back to 30.

--- ISA-CMOP_Python/run_problems.sh
+++ ISA-CMOP_Python/run_problems.sh
@@ -11,7 +11,7 @@ problemsMW=("MW1", "MW2", "MW3", "MW4", "MW5", "MW6", "MW7", "MW8", "MW9", "MW10
 n_dim=(2 5 10)
 
 # Number of samples to run.
-num_samples=5
+num_samples=30
 
 # Modes are debug or eval.
 mode="eval"
