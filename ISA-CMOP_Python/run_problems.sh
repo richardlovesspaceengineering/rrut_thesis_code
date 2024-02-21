6c89129e53c9b6d48054fdc5748324a09b88f954
richardlovesspaceengineering <richardrutherford@outlook.com>
2024-02-21 16:19:53 +1100

    Ready for rerun on megatron2.

--- ISA-CMOP_Python/run_problems.sh
+++ ISA-CMOP_Python/run_problems.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # Message describing the experimental setup.
-desc_msg="Increased dimensionality, now running full benchmark suites (other than aerofoils)."
+desc_msg="Running all 5D, then 10D and so on from JSON file."
 
-# Problem suites
+# Problem suites - these variables do not do anything.
 problemsCTP=("CTP1", "CTP2", "CTP3", "CTP4", "CTP5", "CTP6", "CTP7", "CTP8")
 problemsMW=("MW1", "MW2", "MW3", "MW4", "MW5", "MW6", "MW7", "MW8", "MW9", "MW10", "MW11", "MW12", "MW13", "MW14")
-# problemsMW=("MW14")
 problemsDASCMOP=("DASCMOP1", "DASCMOP2", "DASCMOP3", "DASCMOP4", "DASCMOP5", "DASCMOP6", "DASCMOP7", "DASCMOP8", "DASCMOP9")
 problemsDCDTLZ=("DC1DTLZ1" "DC1DTLZ3" "DC2DTLZ1" "DC2DTLZ3" "DC3DTLZ1" "DC3DTLZ3")
 problemsCDTLZ=("C1DTLZ1" "C1DTLZ3" "C2DTLZ2" "C3DTLZ1" "C3DTLZ4")
@@ -16,14 +15,14 @@ problemsICAS=("ICAS2024Test")
 
 # Dimensions to consider
 # dimensions=(5 10 15 20 30)
-dimensions=(5 10)
+# dimensions=(5 10)
 
 # Number of samples to run.
-num_samples=3
+num_samples=30
 
 # Modes are debug or eval.
-# mode="eval"
-mode="debug"
+mode="eval"
+# mode="debug"
 
 # Use pre-generated samples?
 regenerate_samples=false #@JUAN set to true if you need to generate/can't see the pregen_samples folder as a sibling folder.
