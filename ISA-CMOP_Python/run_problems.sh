7477267c91d0387c24cd0f88abd57fe9b83b3a6f
Richard Rutherford <richardrutherford@outlook.com>
2024-02-22 10:34:35 +1100

    Ready to rerun on megatron.

--- ISA-CMOP_Python/run_problems.sh
+++ ISA-CMOP_Python/run_problems.sh
@@ -21,8 +21,8 @@ problemsICAS=("ICAS2024Test")
 num_samples=30
 
 # Modes are debug or eval.
-# mode="eval"
-mode="debug"
+mode="eval"
+# mode="debug"
 
 # Use pre-generated samples?
 regenerate_samples=false #@JUAN set to true if you need to generate/can't see the pregen_samples folder as a sibling folder.
