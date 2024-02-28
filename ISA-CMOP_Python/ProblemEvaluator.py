8158a5eec610959292f104ec857988b9eb001fc2
richardlovesspaceengineering <richardrutherford@outlook.com>
2024-02-28 17:43:53 +1100

    Debug: only running 1 walk per sample.

--- ISA-CMOP_Python/ProblemEvaluator.py
+++ ISA-CMOP_Python/ProblemEvaluator.py
@@ -554,7 +554,7 @@ class ProblemEvaluator:
         min_values_array = max_values_array
 
         # for j in range(pre_sampler.dim):
-        for j in range(0):
+        for j in range(2):
 
             pop_walk, pop_neighbours_list = self.get_rw_pop(
                 pre_sampler,
