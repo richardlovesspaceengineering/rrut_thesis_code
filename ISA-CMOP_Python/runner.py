e20b6911bc2d944f861ec213bbdd5c3a43ecd65e
richardlovesspaceengineering <richardrutherford@outlook.com>
2024-03-06 16:43:05 +1100

    Returning exit code 1 when failed script.:

--- ISA-CMOP_Python/runner.py
+++ ISA-CMOP_Python/runner.py
@@ -145,4 +145,11 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+
+    try:
+        main()
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+        sys.exit(1)  # Exit with status code 1 to indicate an error
