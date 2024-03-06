01cbbb1f64b7f8b3897a652584de4cefb51c0c5c
richardlovesspaceengineering <richardrutherford@outlook.com>
2024-03-06 15:14:50 +1100

    Fixed naming of n_points in method.

--- ISA-CMOP_Python/cases/LIRCMOP_setup.py
+++ ISA-CMOP_Python/cases/LIRCMOP_setup.py
@@ -54,14 +54,14 @@ class LIRCMOPSetup(Setup):
         cons = self.cons_func_specific(var)
         return obj, cons
 
-    def exact_pareto(self, num_points):
+    def exact_pareto(self, n_points):
         pass
 
     def _calc_pareto_front(self):
         # return super()._calc_pareto_front(
         #     f"./cases/LIRCMOP_files/{self.problem_name}.pf"
         # )
-        return self.exact_pareto(num_points=1000)
+        return self.exact_pareto(n_points=1000)
 
 
 class LIRCMOP1(LIRCMOPSetup):
