46c93773eaeff96bd4acf268f7fc9934ba5fabc2
richardlovesspaceengineering <richardrutherford@outlook.com>
2024-03-06 15:05:32 +1100

    Added debug statement.

--- ISA-CMOP_Python/ProblemEvaluator.py
+++ ISA-CMOP_Python/ProblemEvaluator.py
@@ -1146,10 +1146,10 @@ class ProblemEvaluator:
             + " ~~~~~~~~~~~~ \n"
         )
 
-        rw_features = self.do_random_walk_analysis(
-            self.instance, pre_sampler, eval_pops_parallel=eval_pops_parallel
-        )
-        rw_features.export_unaggregated_features(self.instance_name, "rw", save_arrays)
+        # rw_features = self.do_random_walk_analysis(
+        #     self.instance, pre_sampler, eval_pops_parallel=eval_pops_parallel
+        # )
+        # rw_features.export_unaggregated_features(self.instance_name, "rw", save_arrays)
 
         # Global Analysis.
         print(
