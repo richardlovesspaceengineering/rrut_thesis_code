9876b2bd86a660bdec0758b928c216576dce98af
kj66 <kj66@megatron2>
2024-03-13 22:55:25 +1100

    Set up for run of CF problems from PlatEmo.

--- PlatEMO/PlatEMO_Problems/platemo_interface.py
+++ PlatEMO/PlatEMO_Problems/platemo_interface.py
@@ -5,38 +5,33 @@ import numpy as np
 def call_matlab_functions():
     # Start MATLAB engine
     eng = matlab.engine.start_matlab()
-    # print(eng.path())
     eng.addpath(
-        "CF",
+        eng.genpath("~/Documents/Richard/rrut_thesis_code/PlatEMO/PlatEMO_Problems"),
         nargout=0,
     )
 
     # Create an instance of the CF1 class
-    prob = eng.CF1()
-    eng.Setting(prob)
+    prob = eng.CF1("D", 5)
 
     # Generate some random input for CalObj and CalCon methods
-    X = matlab.double(np.random.rand(5, 10).tolist())  # Adjust the size of X as needed
-    varargin = X
-
-    pf = np.array(eng.GetOptimum(prob, 100))
-    print(pf)
+    X = matlab.double(np.random.rand(5, 5).tolist())  # Adjust the size of X as needed
 
     # Call CalObj method
-    pop_obj = eng.Evaluation(prob, varargin)
-
-    breakpoint()
+    pop_obj = eng.Evaluation(prob, X)
 
     # Display the result
     print("CalObj result:")
-    print(np.array(pop_obj))
+    print(np.array(eng.getfield(pop_obj, "objs")))
 
-    # # Call CalCon method
-    # pop_con = eng.CalCon(prob, X)
+    # Display the result
+    print("CalCon result:")
+    print(np.array(eng.getfield(pop_obj, "cons")))
 
-    # # Display the result
-    # print("CalCon result:")
-    # print(np.array(pop_con))
+    print("PF")
+    print(np.array(eng.GetOptimum(prob, 100)))
+
+    print("Bounds")
+    print(np.array(eng.getfield(prob, "lower")))
 
     # Stop the MATLAB engine
     eng.quit()
